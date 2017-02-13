91e09dad73c7d1245ba324468990ed7323a5c108
Xuefeng Chen <xuefeng.chen@stackpath.com>
2017-02-12 18:56:22 -0800

    init repo

---
+++ README.md
@@ -0,0 +1,17 @@
+### simple waf for discuz to prevent duplicating post submit
+
+# we can also decode body to get formhash, which might be more percise as a hex key components
+
+#how to
+in nginx.conf
+1. set a lua shared DICT called waf inside nginx
+2. initialize a variable called $postctl
+```
+set $postctl 10  <- 10 sec between posts
+```
+3. initialize access control
+```
+access_by_lua_file waf.lua;
+```
+
+a reload is required each time you change $postctl value inside nginx.conf
---
+++ waf.lua
@@ -0,0 +1,26 @@
+local now = ngx.now()
+local request_method = ngx.var.request_method
+local waf = ngx.shared.waf
+local uri = ngx.var.uri
+local postctl = ngx.var.postctl or 10
+if request_method == "POST" and string.find(uri, "forum.php") then
+        local remote_hex = ngx.md5(ngx.var.remote_addr .. "_" .. ngx.var.request_uri)
+        -- ngx.log(ngx.ERR, remote_hex)
+        local lastpost, flags = waf:get(remote_hex)
+        if lastpost then
+                difftime = now - lastpost
+                if difftime <= tonumber(postctl) then
+                        return ngx.redirect(ngx.var.http_referer)
+                else
+                        local succ, err, forcible = waf:set(remote_hex, now)
+                        if err then
+                                ngx.log(ngx.ERR, "failed to store last post info: ", err)
+                        end
+                end
+        else
+                local succ, err, forcible = waf:set(remote_hex, now)
+                if err then
+                        ngx.log(ngx.ERR, "failed to store last post info: ", err)
+                end
+        end
+end
