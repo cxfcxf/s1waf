3cdd2480a2539f247d6d5bfa67e4f5aebe158fe3
Xuefeng Chen <siegfried.chen@gmail.com>
2017-04-09 13:35:14 -0700

    adding a feature which could take blacklist
     by using curl -I "http://ip/waf_blacklist.php?ip=${ip}"

--- waf.lua
+++ waf.lua
@@ -3,7 +3,6 @@ local request_method = ngx.var.request_method
 local waf = ngx.shared.waf
 local uri = ngx.var.uri
 local postctl = ngx.var.postctl or 10
--- also rejecting people using stupid buggy api to achieve double click
 if request_method == "POST" and ( string.find(uri, "forum.php") or string.find(uri, "api/mobile/index") ) then
         local remote_hex = ngx.md5(ngx.var.remote_addr .. "_" .. ngx.var.uri)
         -- ngx.log(ngx.ERR, remote_hex)
@@ -18,6 +17,7 @@ if request_method == "POST" and ( string.find(uri, "forum.php") or string.find(u
                                 ngx.log(ngx.ERR, "failed to store last post info: ", err)
                         end
                 end
+
         else
                 local succ, err, forcible = waf:set(remote_hex, now)
                 if err then
@@ -26,7 +26,18 @@ if request_method == "POST" and ( string.find(uri, "forum.php") or string.find(u
         end
 end
 
-local ua = ngx.req.get_headers()['User-Agent']
-if request_method == "POST" and string.find(ua, "Microsoft URL Control") then
-        ngx.exit(ngx.HTTP_FORBIDDEN)
+if request_method == "POST" then
+        local inlist, flags = waf:get(ngx.var.remote_addr)
+        if inlist then
+                ngx.exit(ngx.HTTP_FORBIDDEN)
+        end
+end
+
+if request_method == "HEAD" and string.find(uri, "waf_blacklist.php") and ngx.var.req.remote_addr == "remote_script_address" then
+        local args = ngx.req.get_uri_args()
+        local succ, err, forcible = waf:set(args["ip"], true)
+        if err then
+                ngx.log(ngx.ERR, "failed to store blacklist: ", err)
+        end
+        ngx.exit(ngx.HTTP_OK)
 end
